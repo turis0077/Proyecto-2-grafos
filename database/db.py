e0a0aacf354fa54c0c83fbe8385b316308049a8a
turis0077 <141362767+turis0077@users.noreply.github.com>
2025-06-01 18:56:47 -0600

    Correction db

--- database/db.py
+++ database/db.py
@@ -14,7 +14,7 @@ class Neo4jConnection:
         uri = os.getenv("NEO4J_URI")
         user = os.getenv("NEO4J_USER")
         password = os.getenv("NEO4J_PASSWORD")
-        if not uri or not user or not pwd:
+        if not uri or not user or not password:
             raise RuntimeError("Faltan variables de entorno para la conexión a Neo4j")
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
